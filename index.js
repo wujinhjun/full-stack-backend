9f042646a7bdd3da2091522d45aaa36b467f2ea5
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-05-18 15:14:11 +0800

    uibuild

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 app.use(cors());
+app.use(express.static("build"));
 
 const weeks = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"];
 
